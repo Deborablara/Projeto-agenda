ad779169b811e1b5e5055cb584fd452c0948c400
Deborablara <debora.blara19@gmail.com>
2022-03-11 21:37:15 -0300

    chore: added hash encryption

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -19,11 +20,19 @@ class Login {
     this.valida()
     if (this.errors.length > 0) return
 
-    try {
-      this.user = await LoginModel.create(this.body)
-    } catch (e) {
-      console.log(e)
-    }
+    await this.userExists()
+
+    if (this.errors.length > 0) return
+
+    const salt = bcryptjs.genSaltSync()
+    this.body.password = bcryptjs.hashSync(this.body.password, salt)
+
+    this.user = await LoginModel.create(this.body)
+  }
+
+  async userExists() {
+    this.user = await LoginModel.findOne({ email: this.body.email })
+    if (this.user) this.errors.push('Este usuário já está cadastrado.')
   }
 
   valida() {
